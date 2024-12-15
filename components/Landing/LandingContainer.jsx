6c18143c2ab136e7b98c21d5eddd8ce1a7b6c723
Shahariarshadhin <shadhin19shahriar@gmail.com>
2024-12-15 12:07:39 +0600

    add service section

--- components/Landing/LandingContainer.jsx
+++ components/Landing/LandingContainer.jsx
@@ -1,9 +1,11 @@
 import HeroBanner from "./Sections/HeroBanner"
+import Services from "./Sections/Services"
 
 const LandingContainer = () => {
     return (
         <div>
             <HeroBanner />
+            <Services />
         </div>
     )
 }
